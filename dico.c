b21a0a6d036780f4c3eec2530e49d4c53c8c2df3
mohamedamara1 <amara@tagamuta-valley.com>
2022-11-20 15:26:16 +0100

    All functions inside dico.c

--- dico.c
+++ dico.c
@@ -1,118 +1,239 @@
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
-#include <time.h>
-
 #include <string.h>
-
-
 
 #include "dico.h"
 
 
 
-int nombreAleatoire(int nombreMax)
+TArbre arbreConsVide(void){
+  return NULL;
+}
 
-{
+int arbreEstVide(TArbre a){
+  return (a==NULL);
+}
 
-    srand(time(NULL));
+TArbre arbreCons(char c, int n, TArbre fg, TArbre fd){
+  TArbre newTree;
+  newTree = (TArbre)malloc(sizeof (struct TElement));
+  newTree->lettre     = c;
+  newTree->nbOcc      = n;
+  newTree->fg = fg;
+  newTree->fd  = fd;
+  return newTree;
+}
 
-    return (rand() % nombreMax);
+char arbreRacineLettre(TArbre a){
+  return a->lettre;
+}
 
+int arbreRacineNbOcc(TArbre a){
+  return a->nbOcc;
+}
+
+TArbre arbreFilsGauche(TArbre a){
+  return a->fg;
 }
 
 
-int piocherMot(char *motPioche)
+TArbre arbreFilsDroit(TArbre a){
+  return a->fd;
+}
+/*
+void arbreSupprUtilDoublePointer(TArbre a){
+    if (!arbreEstVide(a)){
+    arbreSupprUtil(a->fg);
+    arbreSupprUtil(a->fd);
+    free(a);
+  }
+}
 
+void arbreSupprDoublePointer(TArbre* p){
+  arbreSupprUtil(*p);
+  *p = NULL;
+}
+*/
+void arbreSuppr(TArbre a){
+    if (!arbreEstVide(a)){
+    arbreSuppr(a->fg);
+    a->fg = NULL;
+    arbreSuppr(a->fd);
+    a->fd = NULL;
+    free(a);
+  }
+}
+
+/* ------------------------------------------------------- */
+/* Entï¿½tes des fonctions locales ï¿½ cette bibliothï¿½que      */
+/* ------------------------------------------------------- */
+
+void _dicoAfficher(TArbre a, char prefixe[], int pos);
+void _dicoInsererMot(char mot[], int debut, TArbre *pa);
+int  _dicoNbOcc(char mot[], int debut, TArbre a);
+
+
+
+/* ------------------------------------------------------- */
+/* Primitives de gestion d'un dictionnaire                 */
+/* ------------------------------------------------------- */
+
+void dicoAfficher(TArbre a)
 {
+  char buffer[100];
 
-     // Le pointeur de fichier qui va contenir notre fichier
+  _dicoAfficher(a, buffer, 0);
+}
 
-    int nombreMots = 0, numMotChoisi = 0, i = 0;
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-    int caractereLu = 0;
+void dicoInsererMot(char mot[], TArbre *pa)
+{
+  _dicoInsererMot(mot, 0, pa);
+}
 
-    FILE * dico = fopen("input.txt", "r"); // On ouvre le dictionnaire en lecture seule
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-
-
-    // On vérifie si on a réussi à ouvrir le dictionnaire
-
-    if (dico == NULL) // Si on n'a PAS réussi à ouvrir le fichier
-
-    {
-
-        printf("\nImpossible de charger le dictionnaire de mots");
-
-        return 0; // On retourne 0 pour indiquer que la fonction a échoué
-
-        // À la lecture du return, la fonction s'arrête immédiatement.
-
-    }
-
-
-
-    // On compte le nombre de mots dans le fichier (il suffit de compter les
-
-    // entrées \n
-
-    do
-
-    {
-
-        caractereLu = fgetc(dico);
-
-        if (caractereLu == '\n')
-
-            nombreMots++;
-
-    } while(caractereLu != EOF);
-
-
-
-    numMotChoisi = nombreAleatoire(nombreMots); // On pioche un mot au hasard
-
-
-
-    // On recommence à lire le fichier depuis le début. On s'arrête lorsqu'on est arrivé au bon mot
-
-    rewind(dico);
-
-    while (numMotChoisi > 0)
-
-    {
-
-        caractereLu = fgetc(dico);
-
-        if (caractereLu == '\n')
-
-            numMotChoisi--;
-
-    }
-
-
-
-    /* Le curseur du fichier est positionné au bon endroit.
-
-    On n'a plus qu'à faire un fgets qui lira la ligne */
-
-    fgets(motPioche, 100, dico);
-
-
-
-    // On vire le \n à la fin
-
-    motPioche[strlen(motPioche) - 1] = '\0';
-
-    fclose(dico);
-
-
-
-    return 1; // Tout s'est bien passé, on retourne 1
-
+int dicoNbOcc(char mot[], TArbre a)
+{
+  return _dicoNbOcc(mot, 0, a);
 }
 
 
 
+/* ------------------------------------------------------- */
+/* Entï¿½tes des fonctions locales ï¿½ cette bibliothï¿½que      */
+/* ------------------------------------------------------- */
+
+void _dicoAfficher(TArbre a, char prefixe[], int pos)
+{
+  if (!arbreEstVide(a))
+    {
+      prefixe[pos] = arbreRacineLettre(a);
+
+      if (arbreRacineLettre(a) == '\0')
+	printf("\"%s\" [%d]\n", prefixe, arbreRacineNbOcc(a));
+
+      _dicoAfficher(arbreFilsGauche(a), prefixe, pos+1);
+      _dicoAfficher(arbreFilsDroit(a), prefixe, pos);
+    }
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+void _dicoInsererMot(char mot[], int debut, TArbre *pa)
+{
+  if (arbreEstVide(*pa))
+    /* 1er cas: le dictionnaire est vide */
+    {
+      *pa = arbreCons(mot[debut], 0, arbreConsVide(), arbreConsVide());
+      if (debut == strlen(mot))
+	((*pa)->nbOcc)++;
+      else
+	_dicoInsererMot(mot, debut+1, &((*pa)->fg));
+    }
+  else if (mot[debut] < arbreRacineLettre(*pa))
+    /* 2ï¿½me cas: le mot doit ï¿½tre placï¿½ au dï¿½but du dictionnaire */
+    {
+      *pa = arbreCons(mot[debut], 0, arbreConsVide(), *pa);
+      if (debut == strlen(mot))
+	((*pa)->nbOcc)++;
+      else
+	_dicoInsererMot(mot, debut+1, &((*pa)->fg));
+    }
+  else if (mot[debut] > arbreRacineLettre(*pa))
+    /* 3ï¿½me cas: le mot ne commence pas par la mï¿½me lettre que le */
+    /* premier mot (courant) du dictionnaire */
+    {
+      _dicoInsererMot(mot, debut, &((*pa)->fd));
+    }
+  else
+    /* 4ï¿½me cas: le mot ï¿½ insï¿½rer commence par la mï¿½me lettre que */
+    /* le premier mot (courant) du dictionnaire */
+    {
+      if (debut == strlen(mot))
+	((*pa)->nbOcc)++;
+      else
+	_dicoInsererMot(mot, debut+1, &((*pa)->fg));
+    }
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+int _dicoNbOcc(char mot[], int debut, TArbre a)
+{
+  if (arbreEstVide(a))
+    /* 1er cas: le dictionnaire est vide */
+    return 0;
+  else if (debut > strlen(mot))
+    /* 2ï¿½me cas: le mot tel quel n'existe pas */
+    return 0;
+  else if (mot[debut] < arbreRacineLettre(a))
+    /* 3ï¿½me cas: le mot cherchï¿½ aurait dï¿½ ï¿½tre avant dans le dico */
+    return 0;
+  else if (mot[debut] == arbreRacineLettre(a))
+    /* 4ï¿½me cas: le mot commence par la mï¿½me lettre que le premier */
+    /* mot du dictionnaire */
+    if (mot[debut] == '\0')
+      return arbreRacineNbOcc(a);
+    else
+      return _dicoNbOcc(mot, debut+1, arbreFilsGauche(a));
+  else
+    /* 5 ï¿½me cas: le mot est ï¿½ventuellement plus loin dans le dico */
+    return _dicoNbOcc(mot, debut, arbreFilsDroit(a));
+}
+
+/* ------------------------------------------------------- */
+int main(){
+
+  TArbre treeOne = arbreCons('e', 15, NULL, NULL);
+  TArbre treeTwo = arbreCons('f', 16, NULL, NULL);
+  TArbre treeThree = arbreCons('g', 17, treeOne, treeTwo);
+  arbreSuppr(treeTwo);
+  treeTwo = NULL;
+ // arbreSuppr(treeTwo);
+  int vide = arbreEstVide(treeTwo);
+
+
+  TArbre dico;
+  char buffer[100];
+  dico = arbreConsVide();
+  strcpy(buffer, "gallon");
+  dicoInsererMot(buffer, &dico);
+  dicoAfficher(dico);
+  printf("\n");
+
+  strcpy(buffer, "munier");
+  dicoInsererMot(buffer, &dico);
+  dicoAfficher(dico);
+  printf("\n");
+  strcpy(buffer, "abenia");
+  dicoInsererMot(buffer, &dico);
+  dicoAfficher(dico);
+  printf("\n");
+  strcpy(buffer, "munier");
+  dicoInsererMot(buffer, &dico);
+  dicoAfficher(dico);
+  printf("\n");
+  strcpy(buffer, "mumu");
+  dicoInsererMot(buffer, &dico);
+  dicoAfficher(dico);
+  printf("\n");
+  strcpy(buffer, "alpha");
+  dicoInsererMot(buffer, &dico);
+  strcpy(buffer, "alphabeta");
+  dicoInsererMot(buffer, &dico);
+  strcpy(buffer, "al");
+  dicoInsererMot(buffer, &dico);
+  dicoAfficher(dico);
+  printf("\n");
+  printf("\"%s\" \t -> %d\n", "gallon", dicoNbOcc("gallon",dico));
+  printf("\"%s\" \t\t -> %d\n", "mumu", dicoNbOcc("mumu",dico));
+  printf("\"%s\" \t -> %d\n", "munier", dicoNbOcc("munier",dico));
+  printf("\"%s\" \t -> %d\n", "gastro", dicoNbOcc("gastro",dico));
+  printf("\"%s\" \t -> %d\n", "lespine", dicoNbOcc("lespine",dico));
+  printf("\"%s\" \t\t -> %d\n", "aaa", dicoNbOcc("aaa",dico));
+  printf("\n");
+
+  return 0;
+}
